141e8e20b291e43369d72d802f62e5408bf46f52
kavindamai99 <kavindanim@gmail.com>
2021-09-23 23:33:32 +0530

    Update data

--- app/src/main/java/com/example/greenlantern/table/Update_form.java
+++ app/src/main/java/com/example/greenlantern/table/Update_form.java
@@ -10,12 +10,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.greenlantern.MainActivity;
 import com.example.greenlantern.MainAdapter;
 import com.example.greenlantern.R;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Update_form extends AppCompatActivity {
 
@@ -25,7 +30,11 @@ public class Update_form extends AppCompatActivity {
     RecyclerView recyclerView;
 
     TextView tableId,tableName,tableDate,tablePhone,tableTotal;
+    EditText updateName,updatePhoneNum;
+    Button btn_update;
 
+    //database ref for data update
+    DatabaseReference db;
 
 
     @Override
@@ -33,7 +42,7 @@ public class Update_form extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_form);
 
-        //get update key
+        //show update values
         Intent intent = getIntent();
         String key = intent.getStringExtra("editKey");
         String name = intent.getStringExtra("name");
@@ -41,6 +50,10 @@ public class Update_form extends AppCompatActivity {
         String table = intent.getStringExtra("table");
         String total = intent.getStringExtra("total");
         String phone = intent.getStringExtra("phone");
+
+        //fetch data for the update
+        db = FirebaseDatabase.getInstance().getReference("TableD");
+
 
         Log.i("Update key",key);
 
@@ -57,12 +70,36 @@ public class Update_form extends AppCompatActivity {
         tablePhone=findViewById(R.id.tablephone);
         tableTotal=findViewById(R.id.tabletotal);
 
+        //get update details
+        updateName = findViewById(R.id.etUpdateName);
+        updatePhoneNum =findViewById(R.id.etUpdateNumber);
+        btn_update =findViewById(R.id.btn_update);
+
         //set values
         tableId.setText(table);
         tableName.setText(name);
         tableTotal.setText(total);
         tableDate.setText(date);
         tablePhone.setText(phone);
+        updateName.setText(name);
+        updatePhoneNum.setText(phone);
+
+
+        //update data function
+        btn_update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i("name",updateName.toString());
+                Log.i("name",updatePhoneNum.toString());
+
+                //update database
+
+                db.child(key).child("forName").setValue(updateName.getText().toString());
+                db.child(key).child("phoneNum").setValue(updatePhoneNum.getText().toString());
+                Toast.makeText(getApplicationContext(),"Details Update Successfully",Toast.LENGTH_SHORT).show();
+
+            }
+        });
 
 
 
@@ -84,6 +121,10 @@ public class Update_form extends AppCompatActivity {
 
 
 
+    }
+
+    public void back(View view){
+        finish();
     }
 
     @Override
